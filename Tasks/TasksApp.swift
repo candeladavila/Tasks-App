68254b2ee2c765535e603b3b15ca03cbed890457
candeladavila <candeladavila05@gmail.com>
2025-02-07 14:40:29 +0100

    Created Project

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Candela Dávila Moreno on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TasksTests/TasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasksTests.swift
+//  TasksTests
+//
+//  Created by Candela Dávila Moreno on 7/2/25.
+//
+
+import Testing
+@testable import Tasks
+
+struct TasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
